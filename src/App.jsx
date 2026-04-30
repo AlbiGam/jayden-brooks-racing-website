2dafb02f930a6d3ff77070eed0d25767b76afdbe
AlbiGam <138054350+AlbiGam@users.noreply.github.com>
2026-04-30 16:56:09 +0200

    Hotfix: remove router to restore hash section navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
 import Home from './Home.jsx'
-import About from './About.jsx'
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  )
+  return <Home />
 }
 
 export default App
